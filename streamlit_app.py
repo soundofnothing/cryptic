4855eab734135916e45db95f3458c02a45f767f9
soundofnothing <144745171+soundofnothing@users.noreply.github.com>
2023-11-16 13:33:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,45 @@
 
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-def plot_elliptic_curve(a, b):
-    y, x = np.ogrid[-5:5:100j, -5:5:100j]
-    plt.contour(x.ravel(), y.ravel(), y**2 - x**3 - a*x - b, levels=[0], colors='blue')
-    plt.grid()
-    plt.show()
+# def plot_elliptic_curve(a, b):
+#     y, x = np.ogrid[-5:5:100j, -5:5:100j]
+#     plt.contour(x.ravel(), y.ravel(), y**2 - x**3 - a*x - b, levels=[0], colors='blue')
+#     plt.grid()
+#     plt.show()
 
-# Example elliptic curve: y^2 = x^3 - x + 1
-plot_elliptic_curve(-1, 1)
+# # Example elliptic curve: y^2 = x^3 - x + 1
+# plot_elliptic_curve(-1, 1)
+
+# Integer Arithmetic Section in Streamlit App
+import streamlit as st
+
+def integer_arithmetic():
+    st.subheader("Integer Arithmetic")
+
+    # Input fields for two integers
+    num1 = st.number_input("Enter first large integer", format="%d")
+    num2 = st.number_input("Enter second large integer", format="%d")
+
+    # Operation buttons
+    if st.button('Add'):
+        st.write("Result: ", num1 + num2)
+    if st.button('Subtract'):
+        st.write("Result: ", num1 - num2)
+    if st.button('Multiply'):
+        st.write("Result: ", num1 * num2)
+    if st.button('Divide'):
+        st.write("Result: ", num1 / num2 if num2 != 0 else "Cannot divide by zero")
+
+# Main function to run the app
+def main():
+    st.title("Cryptography Concepts Explorer")
+    
+    # Integer Arithmetic Section
+    integer_arithmetic()
+
+    # Other sections will be added here
+
+if __name__ == "__main__":
+    main()
+

5abde0dcc7d9573be2e03f3736088859fdb1e564
soundofnothing <144745171+soundofnothing@users.noreply.github.com>
2023-11-16 14:11:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,59 +11,17 @@
 # # Example elliptic curve: y^2 = x^3 - x + 1
 # plot_elliptic_curve(-1, 1)
 
-# Integer Arithmetic Section in Streamlit App
 import streamlit as st
 
-# TODO - variadic bases (convert numbers between bases and see results of addition and multiplication)
-def integer_arithmetic():
-    st.subheader("Integer Arithmetic")
+# Set page configuration
+st.set_page_config(page_title="Cryptography Playground", page_icon="🔒")
 
-    # Input fields for two integers
-    num1 = st.number_input("Enter first large integer", format="%d")
-    num2 = st.number_input("Enter second large integer", format="%d")
+# Main page content
+st.title("Welcome to the Cryptography Playground")
+st.write("Explore various cryptography concepts through interactive modules.")
 
-    # Operation buttons
-    if st.button('Add'):
-        st.write("Result: ", num1 + num2)
-    if st.button('Subtract'):
-        st.write("Result: ", num1 - num2)
-    if st.button('Multiply'):
-        st.write("Result: ", num1 * num2)
-    if st.button('Divide'):
-        st.write("Result: ", num1 / num2 if num2 != 0 else "Cannot divide by zero")
+# Sidebar (optional)
+st.sidebar.title("Cryptography Modules")
+st.sidebar.write("Select a module from the sidebar to get started.")
 
-# Modular Arithmetic in Streamlit App
-
-def modular_arithmetic():
-    st.subheader("Modular Arithmetic")
-
-    num1 = st.number_input("Enter first number", format="%d", key='mod1')
-    num2 = st.number_input("Enter second number", format="%d", key='mod2')
-    modulus = st.number_input("Enter modulus", min_value=1, step=1, format="%d")
-
-    if st.button('Add'):
-        st.write("Result: ", (num1 + num2) % modulus)
-    if st.button('Subtract'):
-        st.write("Result: ", (num1 - num2) % modulus)
-    if st.button('Multiply'):
-        st.write("Result: ", (num1 * num2) % modulus)
-    if st.button('Modular Inverse'):
-        try:
-            inverse = pow(num2, -1, modulus)
-            st.write("Modular Inverse: ", inverse)
-        except ValueError:
-            st.write("Modular Inverse does not exist")
-
-# Main function to run the app
-def main():
-    st.title("Cryptography Concepts Explorer")
-    
-    # Integer Arithmetic Section
-    integer_arithmetic()
-
-    # Other sections will be added here
-    modular_arithmetic()
-
-if __name__ == "__main__":
-    main()
-
+# Any additional setup or global features can go here
